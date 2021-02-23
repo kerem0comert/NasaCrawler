fa371cf66b4aa0b690aafa75c499bd9176cc28a5
Kerem <kerem0comert@gmail.com>
2021-02-23 18:53:28 +0300

    Init nav graph

--- app/src/main/java/org/keremcomert/nasacrawler/view/FragmentPhotos.kt
+++ app/src/main/java/org/keremcomert/nasacrawler/view/FragmentPhotos.kt
@@ -1,10 +1,33 @@
 package org.keremcomert.nasacrawler.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import org.keremcomert.nasacrawler.R
+import org.keremcomert.nasacrawler.databinding.FragmentPhotosBinding
+import org.keremcomert.nasacrawler.viewmodel.PhotoViewModel
 
 @AndroidEntryPoint
 class FragmentPhotos: Fragment(R.layout.fragment_photos) {
+    private val viewModel by viewModels<PhotoViewModel>()
+    private var b: FragmentPhotosBinding? = null
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        b = FragmentPhotosBinding.bind(view)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView(){
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        b = null
+    }
 
 }
--- app/src/main/java/org/keremcomert/nasacrawler/view/MainActivity.kt
+++ app/src/main/java/org/keremcomert/nasacrawler/view/MainActivity.kt
@@ -2,13 +2,28 @@ package org.keremcomert.nasacrawler.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.keremcomert.nasacrawler.R
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container)
+                as NavHostFragment).findNavController()
+        setupActionBarWithNavController(navController, AppBarConfiguration(navController.graph))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
